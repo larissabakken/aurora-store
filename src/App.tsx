3d70dd2a655a5c6446a1c5faeb7458899f673383
Larissa Ramos <devlarissasr@gmail.com>
2023-03-02 15:11:33 -0300

    creating functinons to separe groups of products

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,61 @@
 import { useState, useEffect } from "react";
+import CardProducts from "./components/CardProducts";
 import ShelfShows from "./components/ShelfConcerts";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [groupPromotion, setGroupPromotion] = useState([]);
+  const [groupOthers, setGroupOthers] = useState([]);
+  const [groupRecords, setGroupRecords] = useState([]);
 
   useEffect(() => {
     fetch("/products.json")
       .then((response) => response.json())
       .then((data) => {
         setProducts(data.products);
-        // handle the response data here
-        console.log(data, "data");
       })
       .catch((error) => console.error(error));
   }, []);
 
-  console.log(products, "products after fetch");
+  useEffect(() => {
+    setGroupPromotion(products.filter((product) => product.promotion === true));
+  }, [products]);
+
+  useEffect(() => {
+    setGroupOthers(products.filter((product) => product.category === "others"));
+  }, [products]);
+
+  useEffect(() => {
+    setGroupRecords(
+      products.filter(
+        (product) =>
+          product.category === "CD" ||
+          product.category === "Vinyl" ||
+          product.category === "Cassette"
+      )
+    );
+  }, [products]);
+
+  console.log(groupPromotion, "groupPromotion");
+  console.log(groupOthers, "groupOthers");
+  console.log(groupRecords, "groupRecords");
+
   return (
     <>
       <div>
-        <h1>Products</h1>
-        <ShelfShows />
+      <div>
+          <h1>Promotions</h1>
+          <CardProducts data={groupPromotion} />
+        </div>
+        {/* <div>
+          <h1>Products</h1>
+          <CardProducts data={products} />
+        </div> */}
+
+        {/* <ShelfShows /> */}
         
-        <ul>
-          
+
+        {/* <ul>
           {products.map((product: any) => (
             <li key={product.id}>
               <h2>{product.name}</h2>
@@ -31,7 +63,7 @@ export default function App() {
               <p>{product.price}</p>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     </>
   );
