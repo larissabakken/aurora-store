6a873fee8c9e14fcc7efdd514653f420ad86ac77
Larissa Ramos <devlarissasr@gmail.com>
2023-03-03 12:44:25 -0300

    add href button tickets

--- src/components/CardConcerts.tsx
+++ src/components/CardConcerts.tsx
@@ -11,6 +11,10 @@ const CardConcerts: React.FC<Props> = ({
   address,
   concertDate,
 }) => {
+  const ulrAurora = "https://www.aurora-music.com/";
+  const ticketStore = () => {
+    window.open(ulrAurora, "_blank");
+  };
   return (
     <div className="bg-card-bg-color my-4 mx-auto sm:mx-10 py-2 rounded-sm flex items-center justify-between px-2">
       <span className="w-1/4 truncate inline-block tooltip" title={description}>
@@ -18,7 +22,11 @@ const CardConcerts: React.FC<Props> = ({
       </span>
       <span className="w-1/4 truncate">{address}</span>
       <span className="w-1/4 truncate">{concertDate}</span>
-      <button className="w-1/4 truncate px-4 py-2 border bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-secondary)] text-white rounded-md shadow-2xl hover:shadow-hover-color hover:scale-110">
+
+      <button
+        onClick={ticketStore}
+        className="w-1/4 truncate px-4 py-2 border bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-secondary)] text-white rounded-md shadow-2xl hover:shadow-hover-color hover:scale-110"
+      >
         Tickets to buy
       </button>
     </div>
